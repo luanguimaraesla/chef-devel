28533f30fd3519de8e019575a4aeed4baa4c91bc
Luan Guimarães <luan@live.in>
2016-03-16 16:46:22 -0300

    Add the squid server configuration recipe.

---
+++ cookbooks/squid/recipes/default.rb
@@ -0,0 +1,35 @@
+# Recipe for squid server
+
+squid_maximum_object_size = "600" #MB
+squid_minimum_object_size = "0" #MB
+squid_cache_size = "30000" #MB
+
+# Yum update
+execute "yum_update" do
+  command "sudo yum update"
+  user "lappis"
+end
+
+# Install Squid
+package 'squid'
+
+# Configure squid.conf
+squid_conf_file = "/etc/squid/squid.conf"
+template squid_conf_file do
+  source 'squid.conf.erb'
+  variables({
+    cache_size: squid_cache_size,
+    maximum_object_size: squid_maximum_object_size,
+    minimum_object_size: squid_minimum_object_size
+  })
+end
+
+# Enable iptables to listen port 3128
+execute "add_squid_iptables_rule" do
+  command "iptables -I INPUT -p tcp --dport 3128 -j ACCEPT && " +
+          "iptables-save > /etc/sysconfig/iptables"
+end
+
+service "iptables" do
+  action :restart
+end
