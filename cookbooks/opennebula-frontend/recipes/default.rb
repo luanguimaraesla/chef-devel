8cd2f6420267feccec4794bef9bcff523e3a1c85
MaxAlmeida <llewxam100@hotmail.com>
2016-03-30 16:07:20 +0000

    Fixing some erros and add new commands

--- cookbooks/opennebula-frontend/recipes/default.rb
+++ cookbooks/opennebula-frontend/recipes/default.rb
@@ -13,28 +13,32 @@ end
 package 'opennebula-server'
 package 'opennebula-sunstone'
 
+execute 'disable-selinux' do
+  command 'sed -i "s/SELINUX=\w*/SELINUX=disable /g" etc/sysconfig/selinux'
+end
 
 execute 'change_script' do
   command 'sed -i "s/yum install/yum install -y/g" /usr/share/one/install_gems'
 end
 
 execute 'sunstone' do
-  command 'echo -e 1 "\n" | /usr/share/one/install_gems'
+  command 'echo -e 1 "\n" "\n"| /usr/share/one/install_gems'
 end
 
-service 'opennebula-server' do 
-  action [:enable, :start]
+execute 'external_acess'
+  command 'sed -i "s/:host: 127.0.0.1/:host: 0.0.0.0/g" /etc/one/sunstone-server.conf'
 end
+
 
 service 'opennebula-sunstone' do
   action [:enable, :start]
 end
  
-execute "ssh_public_key_permission" do
-  command "cat << EOT > ~/.ssh/config"+
-          "Host *
-              StrictHostKeyChecking no
-              UserKnownHostsFile /dev/null"+
-          "EOT"+
-          "chmod 600 ~/.ssh/config" 
-end
+#execute "ssh_public_key_permission" do
+ # command "cat << EOT > ~/.ssh/config"+
+  #        "Host *
+   #           StrictHostKeyChecking no
+    #          UserKnownHostsFile /dev/null"+
+     #     "EOT"+
+      #    "chmod 600 ~/.ssh/config" 
+#end
