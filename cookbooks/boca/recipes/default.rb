57a334a5b300c7786a24bcba2fdd9265a248dd09
Luan Guimarães <luan@live.in>
2016-03-31 16:57:14 -0300

    Add the initial boca default recipe file.

--- cookbooks/boca/recipes/default.rb
+++ cookbooks/boca/recipes/default.rb
@@ -1,14 +1,31 @@
+# Configurations
 url_download_installation_file = "http://www.ime.usp.br/~cassio/boca/download.php?filename=installv2.sh"
 work_dir = "/home/lappis/"
 boca_installation_file = "install_boca.sh"
 
+# Download installer
 remote_file work_dir + boca_installation_file do
   source url_download_installation_file
+  mode '0755'
 end
 
-bash 'extract_module' do
-  cwd work_dir
-  code <<-EOH
-    ./install_boca.sh
-  EOH
+# Update apt-get
+execute "apt-get_update" do
+  command "apt-get -y update"
 end
+
+# Execute installer script
+execute 'boca_script' do
+  flags = " alreadydone"
+  command boca_installation_file + flags
+  cwd work_dir
+end
+
+# Reboot virtual machine
+reboot 'now' do
+  action :restart_now
+  reason 'Boca needs to reboot'
+  delay_mins 1
+end
+
+
