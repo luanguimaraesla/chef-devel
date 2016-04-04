91158fd92f50bb0657abf429bc32315c7ae19fd8
MaxAlmeida <llewxam100@hotmail.com>
2016-04-04 02:48:23 +0000

    Iproving redmine recipe

--- cookbooks/redmine/recipes/default.rb
+++ cookbooks/redmine/recipes/default.rb
@@ -1,9 +1,16 @@
 version = '3.2.0'
 url_redmine = "https://www.redmine.org/releases/redmine-#{version}.tar.gz"
 redmine_dir = '/opt/redmine/'
-
+apache_conf_dir = '/etc/apache2/sites-available/master.conf'
 #install dependencies
+execute 'update' do
+  command "apt-get update"
+  ignore_failure true
+  action :nothing
+end
 packages = %w(mysql-client libmysqlclient-dev gcc build-essential zlib1g zlib1g-dev zlibc ruby-zip libssl-dev libyaml-dev libcurl4-openssl-dev ruby gem libapache2-mod-passenger apache2-mpm-prefork apache2-dev libapr1-dev libxslt1-dev checkinstall libxml2-dev ruby-dev vim libmagickwand-dev imagemagick)
+
+#gem_package 'passenger'
 packages.each do |package_name|
   package package_name
 end
@@ -26,7 +33,7 @@ gem_package 'bundler'
 extracted_redmine_dir = redmine_dir+'redmine-'+version
 
 execute 'bundle_install' do
-  command 'bundle install --without development test'
+  command 'bundle install'
   cwd extracted_redmine_dir
 end
 
@@ -53,4 +60,33 @@ execute "chown-data-www" do
   user "root"
   cwd extracted_redmine_dir
 end
- 
+
+execute "simbolic_link" do
+  command 'sudo ln -s '+extracted_redmine_dir+'/public/'+' /var/www/html/redmine'  
+end 
+
+template apache_conf_dir do
+  source 'master.conf.erb'
+end
+
+execute 'disable_default_apache' do
+  command 'sudo a2dissite 000-default.conf'
+end
+
+execute 'enable_master_conf' do
+  command 'sudo a2ensite master.conf'
+end
+
+execute 'passenger_permission' do
+  command "echo 'PassengerUser www-data' >> /etc/apache2/mods-available/passenger.conf"
+end
+
+execute 'enable_passenger' do
+  command "sudo a2enmod passenger"
+
+end
+service 'apache2' do
+  action :restart
+end 
+
+
