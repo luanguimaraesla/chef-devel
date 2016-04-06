4f1766afc52cd28d26ac0c9dcdb6d06b6935013d
jessicacris <jessica.cris1127@gmail.com>
2016-04-06 11:56:16 -0300

    Adding firewall recipe

---
+++ cookbooks/firewall/recipes/default.rb
@@ -0,0 +1,24 @@
+iptables_dir = "/etc/iptables.up.rules" 
+
+#Update apt-get
+execute "enable_IP_forwading " do
+  command "sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf"
+end
+
+#Execute forwading ip
+execute "forwading ip"
+  command "sudo sysctl -p"
+
+template iptables_dir do
+  source "iptables.up.rules.erb"
+end
+
+#Enable script
+execute "enable scripts" do
+  command "chmod +x /etc/iptables.up.rules"
+end
+
+#Execute script
+execute "run scripts" do
+  command "./etc/iptables.up.rules"
+end
