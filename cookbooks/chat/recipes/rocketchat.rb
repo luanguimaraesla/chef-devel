fa3679960aac795132890a3018cd5db029d19a01
Luan Guimarães <luan@live.in>
2016-07-25 17:56:18 -0300

    Fix some errors @ rocketchat recipe (running)
    Signed-off-by: Luan Guimarães <luan@live.in>
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

--- cookbooks/chat/recipes/rocketchat.rb
+++ cookbooks/chat/recipes/rocketchat.rb
@@ -21,7 +21,8 @@ execute "update apt" do
   command "apt-get update"
 end
 
-packages = %w(mongodb-org curl graphicsmagick npm nodejs build-essential)
+
+packages = %w(tmux mongodb-org curl graphicsmagick npm nodejs build-essential)
 
 packages.each do |p|
   package p
@@ -35,13 +36,20 @@ execute "change node version to the least" do
   command "n 0.10.40"
 end
 
-execute "install tool to change node version" do
-  command "npm install -g node-gyp"
+# Configure host alias for mongo
+execute "remove wrong hostname alias" do
+  command 'sed -i "/127\.0\.1\.1/d" /etc/hosts'
+  ignore_failure true
+end
+
+execute "alias hostname" do
+  command 'sed -i "s/127\.0\.0\.1/127\.0\.0\.1\trocketchat/g" /etc/hosts'
+  not_if 'grep -q rocketchat /etc/hosts'
 end
 
 execute "configure mongo" do
-  command 'echo "replication:\n\treplSetName:  \"001-rs\"" >> /etc/mongod.conf'
-  not_if 'echo -o -P "replSetName:" /etc/mongod.conf | wc -l'
+  command 'echo "replication:\n  replSetName:  \"001-rs\"" >> /etc/mongod.conf'
+  not_if 'grep -q 001-rs /etc/mongod.conf'
 end
 
 service "mongod" do
@@ -55,10 +63,9 @@ end
 execute "add mongo environment variable" do
   command 'echo "export MONGO_OPLOG_URL=mongodb://localhost:27017/local" >> /etc/environment'
   command 'export MONGO_OPLOG_URL=mongodb://localhost:27017/local'
-  not_if 'echo -o -P "MONGO_OPLOG_URL" /etc/environment | wc -l'
+  not_if 'grep -q "MONGO_OPLOG_URL" /etc/environment'
 end
 
-ENV['MONGO_OPLOG_URL'] = "mongodb://localhost:27017/local"
 
 # INSTALL ROCKET.CHAT
 
@@ -66,26 +73,39 @@ execute "download stable version of rocket.chat" do
   command "curl -L https://rocket.chat/releases/latest/download -o /root/rocket.chat.tgz"
 end
 
+
 execute "untar the binary release" do
   cwd "/root"
   command "tar zxvf rocket.chat.tgz"
-  command "rm rocket.chat.tgz"
+#  command "rm rocket.chat.tgz"
 end
 
 execute "add mongo environment variable" do
   command "echo \"export ROOT_URL=#{external_address}\" >> /etc/environment"
   command 'echo "export MONGO_URL=mongodb://localhost:27017/rocketchat" >> /etc/environment'
   command 'echo "export PORT=80" >> /etc/environment'
-  not_if 'echo -o -P "ROOT_URL" /etc/environment | wc -l'
+  not_if 'grep -q "ROOT_URL" /etc/environment'
 end
 
-ENV['ROOT_URL'] = external_address
-ENV['MONGO_URL'] = "mongodb://localhost:27017/rocketchat"
-ENV['PORT'] = "80"
+execute "remove old Rocket.Chat dir" do
+  command "rm -rf /root/Rocket.Chat/"
+end
+
+execute "fix npm missing package" do
+  command "npm install fibers@1.0.5 -g"
+end
 
 execute "rename Rocket.Chat directory" do
   cwd "/root/"
   command "mv bundle Rocket.Chat"
+end
+
+execute "mkdir of npm modules" do
+  command "mkdir -p /root/Rocket.Chat/programs/server/node_modules/fibers/"
+end
+
+execute "copy fiber binary" do
+  command "cp -ar /usr/local/lib/node_modules/fibers/ /root/Rocket.Chat/programs/server/node_modules/"
 end
 
 execute "install Rocket.Chat" do
@@ -94,6 +114,11 @@ execute "install Rocket.Chat" do
 end
 
 execute "run Rocket.Chat" do
-  cwd "/root/Rocker.Chat"
-  command "node main.js"
+  ENV['ROOT_URL'] = external_address
+  ENV['MONGO_URL'] = "mongodb://localhost:27017/rocketchat"
+  ENV['PORT'] = "80"
+  ENV['MONGO_OPLOG_URL'] = "mongodb://localhost:27017/local"
+  cwd "/root/Rocket.Chat"
+  command "tmux new-session -s rocketchat -d &&
+	   tmux send-keys -t rocketchat 'node main.js' enter"
 end
