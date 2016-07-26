cb9cd79a6076a4d5670f6e64eb1c4d246c859486
Luan Guimarães <luan@live.in>
2016-07-26 16:30:09 -0300

    Fixing rocketchat template

--- cookbooks/chat/attributes/rocketchat.rb
+++ cookbooks/chat/attributes/rocketchat.rb
@@ -1,4 +1,4 @@
-default[:rocketchat][:initd][:external_address] = "http://mattermost.lappis.rocks/"
-default[:rocketchat][:initd][:mongo_url] = "mongodb://localhost:27017/rocketchat"
-default[:rocketchat][:initd][:port] = "3000"
-default[:rocketchat][:initd][:mongo_oplog_url] = "mongodb://localhost:27017/local"
+rocketchat[:rocketchat][:initd][:external_address] = "http://mattermost.lappis.rocks/"
+rocketchat[:rocketchat][:initd][:mongo_url] = "mongodb://localhost:27017/rocketchat"
+rocketchat[:rocketchat][:initd][:port] = "3000"
+rocketchat[:rocketchat][:initd][:mongo_oplog_url] = "mongodb://localhost:27017/local"
