18da3ec9634c2b0b010901666a7358d28e0bb4e9
Lucas Severo <lucassalves65@gmail.com>
2016-07-28 18:01:56 -0300

    fix order of installs
    Signed-off-by: Lucas Severo <lucassalves65@gmail.com>

--- cookbooks/chat/recipes/hubot.rb
+++ cookbooks/chat/recipes/hubot.rb
@@ -1,14 +1,21 @@
+package 'git'
+
 git '/root/hubot-rocketchat' do
   repository 'https://github.com/RocketChat/hubot-rocketchat.git'
   revision 'master'
   action :sync
 end
 
-package 'docker'
-
 execute 'enable jessie backport' do
   command 'echo "deb http://http.debian.net/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list'
 end
+
+execute 'purge other docker repos' do
+  command 'apt-get purge lxc-docker*'
+  command 'apt-get purge docker.io*'
+end
+
+package 'docker.io'
 
 execute 'apt-get update'
 
