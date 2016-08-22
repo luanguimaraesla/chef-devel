3607fa3ed2c922ad38374c5fdc2b6b631d5ed4d5
Paulo Tada <paulohtfs@gmail.com>
2016-08-22 16:32:22 -0300

    Configuring each nginx service file

--- config/roles/letsencrypt_server.rb
+++ config/roles/letsencrypt_server.rb
@@ -3,5 +3,6 @@ description "Install and configure letsencrypt server"
 
 run_list *[
   'recipe[basics]',
-  'recipe[letsencrypt]'
+  'recipe[letsencrypt]',
+  'recipe[letsencrypt::nginx_servers]'
 ]
