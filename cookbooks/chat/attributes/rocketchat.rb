327fee8351719d4fdbb158a100dca624f497fede
Lappis <lappis@services>
2016-08-26 16:05:22 +0000

    Configuring hubot docker environment
    Signed-off-by: Luan Guimarães <luan@live.in>
    Signed-off-by: Arthur Jahn <stutrzbecher@gmail.com>

--- cookbooks/chat/attributes/rocketchat.rb
+++ cookbooks/chat/attributes/rocketchat.rb
@@ -1,4 +1,4 @@
-default[:rocketchat][:initd][:external_address] = "http://chat.lappis.rocks/"
+default[:rocketchat][:initd][:external_address] = "https://chat.lappis.rocks/"
 default[:rocketchat][:initd][:mongo_url] = "mongodb://localhost:27017/rocketchat"
 default[:rocketchat][:initd][:port] = "3000"
 default[:rocketchat][:initd][:mongo_oplog_url] = "mongodb://localhost:27017/local"
--- cookbooks/chat/recipes/hubot.rb
+++ cookbooks/chat/recipes/hubot.rb
@@ -15,11 +15,23 @@ file '/etc/apt/sources.list.d/backports.list' do
 end
 
 execute 'purge other docker repos' do
-  command 'apt-get purge lxc-docker*'
-  command 'apt-get purge docker.io*'
+  command 'apt-get purge lxc-docker* -y'
+  command 'apt-get purge docker.io* -y'
 end
 
 
 execute 'apt-get update'
 
 package 'docker.io'
+
+execute 'docker run hubot' do
+	command "docker run -i -e ROCKETCHAT_URL=http://#{ node['peers']['rocketchat'] }:3000 \
+    -e ROCKETCHAT_ROOM= \
+    -e LISTEN_ON_ALL_PUBLIC=true \
+    -e ROCKETCHAT_USER=botson \
+    -e ROCKETCHAT_PASSWORD=#{ node['passwd']['hubot'] } \
+    -e ROCKETCHAT_AUTH=password \
+    -e BOT_NAME=hubot \
+    -e EXTERNAL_SCRIPTS=hubot-pugme,hubot-help \
+    rocketchat/hubot-rocketchat"
+end
