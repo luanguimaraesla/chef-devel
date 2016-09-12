d9380be5e1965086cd79c452fb2d3afb86f5946f
Paulo Tada <paulohtfs@gmail.com>
2016-09-12 10:32:19 -0300

    Adding the portal_server role

---
+++ config/roles/portal_server.rb
@@ -0,0 +1,9 @@
+name "portal_server"
+description "Install and configure Portal"
+
+run_list *[
+  'recipe[postfix]',
+  'recipe[postgresql::service]',
+  'recipe[portal::database]',
+  'recipe[portal::noosfero]',
+]
