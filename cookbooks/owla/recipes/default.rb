7fc6a93817ac3f08fa627a22f14e92a4cc34ab4b
Luan Guimarães <livreluan@gmail.com>
2016-11-06 07:10:47 -0200

    Adding new debian packages required to owla's gems

--- cookbooks/owla/recipes/default.rb
+++ cookbooks/owla/recipes/default.rb
@@ -1,17 +1,11 @@
-# Install and configure docsplit
-# Luan Guimarães e Thalisson Barreto
-# Oct 28 2016
-
-# Remember to put docsplit gem @ app's Gemfile
-
-necessary_packages = %w(graphicsmagick poppler-utils poppler-data ghostscript tesseract-ocr pdftk)
-
-necessary_packages.each do | pkg |
-  package pkg
-end
-
 # Install imagemagick
 # Luan Guimarães
 # Oct 29 2016
 
 package 'imagemagick'
+
+# Install rmagick
+# Luan Guimarães
+# Nov 04 2016
+
+package 'libmagickwand-dev'
